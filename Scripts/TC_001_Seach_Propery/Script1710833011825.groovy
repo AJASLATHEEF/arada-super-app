e66a018f430c3964104e6cd497dae501002a8d49
ajas <ajas@LapWinX-DAC5.arackal.co.in>
2024-03-28 13:28:09 +0530

    first commit

---
+++ Scripts/TC_001_Seach_Propery/Script1710833011825.groovy
@@ -0,0 +1,156 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
+import java.time.LocalDate as LocalDate
+import java.time.LocalDateTime as LocalDateTime
+import java.time.format.DateTimeFormatter as DateTimeFormatter
+import org.openqa.selenium.By as By
+import io.appium.java_client.AppiumDriver as AppiumDriver
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import io.appium.java_client.MobileElement as MobileElement
+
+Date todaysDate = new Date()
+
+def formattedDate = todaysDate.format('yyyy/MM/dd')
+
+DateTimeFormatter screenFormat = DateTimeFormatter.ofPattern('yyyy/MM/dd')
+
+DateTimeFormatter screenFormat1 = DateTimeFormatter.ofPattern('dd')
+
+LocalDate localDate = LocalDate.now()
+
+def gTomorrowDate = localDate.plusDays(1).format(screenFormat)
+
+def next_day = localDate.plusDays(1).format(screenFormat1)
+
+String tomorw = next_day.toString()
+
+println('next day is ' + next_day)
+
+println(formattedDate)
+
+Mobile.startApplication('C:\\Users\\ajas\\Downloads\\app-staging-release.apk', true)
+
+WebUI.delay(25)
+
+Mobile.takeScreenshotAsCheckpoint('Launch Screen')
+
+Mobile.takeScreenshot()
+
+Mobile.sendKeys(findTestObject('Search_Property/Email Text Field'), GlobalVariable.Username)
+
+WebUI.delay(5)
+
+Mobile.sendKeys(findTestObject('Search_Property/Password Text Field'), GlobalVariable.Valid_Password)
+
+Mobile.delay(5)
+
+Mobile.tap(findTestObject('Invalid Password/Login Button'), 0)
+
+Mobile.delay(13)
+
+Mobile.takeScreenshotAsCheckpoint('After Login')
+
+Mobile.takeScreenshot()
+
+Mobile.getText(findTestObject('Search_Property/Welcome title'), 0)
+
+Mobile.tap(findTestObject('Search_Property/Explore Property Tile'), 0)
+
+Mobile.tap(findTestObject('Search_Property/Sort Icon'), 0)
+
+Mobile.delay(8)
+
+Mobile.takeScreenshotAsCheckpoint('After Click Sort Icon')
+
+Mobile.takeScreenshot()
+
+Mobile.delay(8)
+
+Mobile.tap(findTestObject('Search_Property/Masaar Tile'), 0)
+
+Mobile.delay(8)
+
+Mobile.switchToNative()
+
+Mobile.tap(findTestObject('Search_Property/Select Property Type'), 0)
+
+Mobile.delay(7)
+
+Mobile.switchToNative()
+
+Mobile.tap(findTestObject('Search_Property/Select Cluster Drop'), 0)
+
+Mobile.delay(7)
+
+Mobile.tap(findTestObject('Search_Property/Robinia Cluster Type Drop Value'), 0)
+
+Mobile.delay(7)
+
+Mobile.tap(findTestObject('Search_Property/Select Project Drop'), 0)
+
+Mobile.delay(3)
+
+Mobile.tap(findTestObject('Search_Property/Robinia Project Type Drop Value'), 0)
+
+Mobile.scrollToText('Select Bedroom')
+
+Mobile.tap(findTestObject('Search_Property/Click Bedroom Count 4'), 0)
+
+Mobile.scrollToText('View Result')
+
+WebUI.delay(5)
+
+//Click View Result Button
+Mobile.tapAtPosition(367, 1319)
+
+Mobile.delay(5)
+
+Mobile.takeScreenshotAsCheckpoint('After Click Search button')
+
+Mobile.takeScreenshot()
+
+//Click First search result
+Mobile.tap(findTestObject('Search_Property/Click First Search Result'), 0)
+
+//Sroll to 'Book a Visit' buttton
+Mobile.scrollToText('Book a Visit')
+
+Mobile.switchToNative()
+
+Mobile.tapAtPosition(367, 1353)
+
+WebUI.delay(8)
+
+Mobile.takeScreenshotAsCheckpoint('Select date')
+
+Mobile.takeScreenshot()
+
+AppiumDriver<?> driver = MobileDriverFactory.getDriver()
+
+MobileElement el = driver.findElement(By.xpath(('//android.widget.TextView[@text=\'' + tomorw) + '\']'))
+
+if (el.getText().equals(tomorw)) {
+    el.click()
+}
+
+Mobile.tap(findTestObject('Search_Property/Click Time'), 0)
+
+Mobile.tapAtPosition(367, 1353)
+
+String alert = Mobile.getText(findTestObject('Search_Property/Booking Successful alert message'), 0)
+
+Mobile.takeScreenshotAsCheckpoint('After Schedule button click')
+
+Mobile.takeScreenshot()
+
+if (alert.contains(gTomorrowDate)) {
+    println('Alert message is matching' // Log a failure message and stop the execution
+        )
+} else {
+    throw new RuntimeException('Automation stopped because the condition is not satisfied.')
+}
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Arada Super App Suite_1.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Arada Super App Suite_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>2</numberOfRerun>
+   <pageLoadTimeout>40</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bd3426c9-6a3d-48cf-8e54-3f3593568687</testSuiteGuid>
+   <testCaseLink>
+      <guid>8562b09a-a7cc-4244-98a7-c3d885bb3486</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002_Invalid_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b9d71556-662f-4993-9911-e71cadbc7817</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a4bdf033-c9f7-41b0-b793-ab3faad79966</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2192e641-34df-4fd7-88fd-828723878c50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001_Seach_Propery</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>cd1e2278-0f44-4707-bb2f-64c5d43e4409</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5dd408f1-19ac-4b2a-a9e4-f981d98bba31</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
