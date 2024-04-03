f25d6bd31a1eb2be0e56474ef3dd2a1e086a1a82
ajas <ajas@LapWinX-DAC5.arackal.co.in>
2024-04-03 13:07:57 +0530

    commit 1.1

---
+++ Test Suites/Suite 3.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite 3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c2e40844-fe93-44ac-b20f-021586b31d4d</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b35607f-2ae3-4ca0-9554-9fe1d1254f87</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001A_Search_Property</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
