32192f7a277e0f8a29216a7c1283ffff7eca8dcb
ajas <ajas@LapWinX-DAC5.arackal.co.in>
2024-04-24 13:40:30 +0530

    commit

--- Test Suites/Suite 2.ts
+++ Test Suites/Suite 2.ts
@@ -12,10 +12,22 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>2da8b190-ed51-4b7b-9965-7f9dbb93f403</testSuiteGuid>
    <testCaseLink>
-      <guid>94a46b66-7111-442b-a519-b0b984882d32</guid>
+      <guid>4a08c60f-9ed3-4b7f-b429-d29f3072110b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_001_Seach_Propery</testCaseId>
+      <testCaseId>Test Cases/TC_002_Invalid_Login</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b9d71556-662f-4993-9911-e71cadbc7817</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a4bdf033-c9f7-41b0-b793-ab3faad79966</variableId>
+      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
